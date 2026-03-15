0c38e4c212dd5713b116eae782e58b0c37b5f644
staticlock <2133525474@qq.com>
2026-03-15 13:43:48 +0800

    增加部署

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  // 部署相关的配置
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/Front-end/" // 改成你的 GitHub 仓库名
+      : "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
